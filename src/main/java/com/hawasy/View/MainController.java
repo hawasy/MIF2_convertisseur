247e47c9afdd995832d4c8b3139feab97377d158
hawasy <hawasy94@hotmail.fr>
2015-12-02 20:45:52 +0100

    Convertisseur

---
+++ src/main/java/com/hawasy/View/MainController.java
@@ -0,0 +1,13 @@
+package com.hawasy.View;
+
+/**
+ * Created by Hawa on 26/11/2015.
+ */
+
+import com.hawasy.model.Index;
+
+public class MainController {
+
+    //Crée un index avec les devises à convertir
+   Index indexDevise = new Index("liste des devises",1);
+}
